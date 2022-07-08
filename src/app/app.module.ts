7c928751bf7c5edeabe9700f1d40f7c5eff8e2ef
gautham1976 <gautham1976@gmail.com>
2022-07-08 10:02:14 +0530

    Name initialized

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ViewstudentComponent } from './viewstudent/viewstudent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewstudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
